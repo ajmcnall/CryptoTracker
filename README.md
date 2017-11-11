4f5f4ab49dd38d795f3dabbde21c244b55da9649
Andy McNally <ajmcnall@umich.edu>
2017-11-11 03:14:39 -0500

    adding mysql support and stuff

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # CryptoTracker
 Tracks cryptocurrencies via gdax api
 https://docs.gdax.com
+
+# Setup
+Utilizes pipenv, "pip install pipenv"
+Then "pipenv install" to install the packages required, located in Pipfile
+This runs packages inside a virtualenv.  Alternatively you can just install
+the packages listed in Pipfile using pip.
+
+Must have a running mysql server
+
+# Running
+If pipenv was used, run using "pipenv run python main.py"
+
+Otherwise "python main.py"
--- main.py
+++ main.py
@@ -9,17 +9,28 @@ import sys
 price = gdaxAPI.getPrice('ETH')
 
 capital = 10000;
-shares = [];  # find better name
+trades = [];  # find better name
 
-shares[0] = {}
-shares['price'] = price
-shares['coins'] = capital/price
+trades[0] = {}
+trades['type'] = 'buy'
+trades['price'] = price
+trades['coins'] = capital/price
 
 
 # buy everything.
 
 while(1):
-	time.sleep(4)
-	newPrice = gdaxAPI.getPrice('ETH')
-	
+	time.sleep(1)
+	currentPrice = gdaxAPI.getPrice('ETH')
+	for trade in trades:
+		if trade.type == 'buy':
+			if currentPrice > trade.price * 1.01: # 1% increase
+				trade.type = 'sell'
+				trade.price = currentPrice
+
+		else if trade.type == 'sell':
+			if currentPrice < trade.price * .99
+				trade.type = 'buy'
+				trade.price = currentPrice
+				trade.coins = trade.price / currentPrice
 
