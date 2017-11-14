bab727a8fdc2d5d89bf9861e966410846ed0a998
Andy McNally <ajmcnall@umich.edu>
2017-11-13 23:26:48 -0500

    added skeleton for decider class

---
+++ decider.py
@@ -0,0 +1,14 @@
+#functions for deciding what to do
+
+#returns true if should buy
+def buyOnPercent(coinPrice, capital, percent):
+	
+	return true
+
+
+
+#returns true if should sell
+def buyOnPercent(coinPrice, oldCoinPrice, percent):
+	
+	return true
+
